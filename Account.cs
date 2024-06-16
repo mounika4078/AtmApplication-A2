adf511276b6475ca52e3e3546958da76c2a79897
mounika4078 <Mgundu4078@conestogac.on.ca>
2024-06-15 22:27:43 -0400

    Added-AccountClass

---
+++ Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtmApplication
+{
+    internal class Account
+    {
+        // Account  Properties
+        public int AccountNumber { get;  set; }
+        public decimal Balance { get;  set; }
+        public decimal InterestRate { get;  set; } 
+        public string AccountHolderName { get;  set; }
+        public List<string> Transactions { get;  set; }
+
+       
+    }
+}
+
